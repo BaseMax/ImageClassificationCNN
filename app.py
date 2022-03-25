4aca64e47a754556b5f7500a4dbf818eb25efe07
Max Base <MaxBaseCode@gmail.com>
2022-03-26 01:54:30 +0430

    improve and test training model

--- app.py
+++ app.py
@@ -25,9 +25,10 @@ print('Shape of Y_test: ', Y_test.shape)
 
 print(X_train[1,:])
 
+# randomly show an image
 idx = random.randint(0, len(X_train))
-plt.imshow(X_train[idx, :])
-plt.show()
+# plt.imshow(X_train[idx, :])
+# plt.show()
 
 # model = Sequential([
 #     Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
@@ -54,7 +55,7 @@ model.add(Dense(64, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # opt = keras.optimizers.SGD(learning_rate=0.001)
-model.compile(loss = 'binary_crossentryopy', optimizer = 'adam', metrics = ['accuracy'])
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 # model.fit(X_train, Y_train, epochs = 10, batch_size = 32)
 model.fit(X_train, Y_train, epochs = 5, batch_size = 64)
@@ -62,18 +63,21 @@ model.fit(X_train, Y_train, epochs = 5, batch_size = 64)
 model.evaluate(X_test, Y_test)
 
 # Making predictions
-idx = random.randint(0, len(Y_test))
-plt.imshow(X_test[idx2, :])
-plt.show()
+for i in range(0, 5):
+    idx2 = random.randint(0, len(Y_test))
+    plt.imshow(X_test[idx2, :])
+    plt.show()
 
-y_pred = model.predict(X_test[idx2, :].reshape(1, 100, 100, 3))
-print(y_pred)
-y_pred = y_pred > 0.5
-print(y_pred)
+    y_pred = model.predict(X_test[idx2, :].reshape(1, 100, 100, 3))
+    print(y_pred)
+    y_pred = y_pred > 0.5
+    print(y_pred)
 
-if y_pred == 0:
-    pred = 'dig'
-else
-    pred = 'cat'
+    if y_pred == False:
+        pred = 'dog'
+    else:
+        pred = 'cat'
 
-print("Our model predicts that the image is a " + pred)
+    print("Our model predicts that the image is a " + pred)
+    print("The actual label is " + str(Y_test[idx2]))
+    print("\n")
